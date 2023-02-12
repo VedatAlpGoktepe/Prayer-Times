2d7d0f79fcafd0afdf4c3bfdd41192fe51820f3a
Vedat Alp Goktepe <vedatalpgktp@gmail.com>
2023-02-12 17:02:53 -0500

    version check

--- PrayerOnline.java
+++ PrayerOnline.java
@@ -7,6 +7,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
+import javax.swing.BorderFactory;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,10 +19,10 @@ public class PrayerOnline {
 	public static void main(String[] args) throws Exception
 	{
 		JFrame window = new JFrame("Prayer Times");
-		window.setSize(300,300);
+		window.setSize(300,325);
 		window.setVisible(true);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setResizable(false);
+		window.setResizable(true);
 
 		JPanel panel = new JPanel();
 
@@ -27,45 +30,79 @@ public class PrayerOnline {
 		times.setText("hello");
 		times.setFont(new Font("Arial", Font.PLAIN, 30));
 
+		String[] calcTypes = {"ISNA", "Diyanet"};
 		String[] places = {"Toronto", "Ottawa"};
 
+		JComboBox calc = new JComboBox(calcTypes);
+		calc.setPreferredSize(new Dimension(120, 40));
+		calc.setFont(new Font("Arial", Font.PLAIN, 18));
 		JComboBox location = new JComboBox(places);
+		location.setPreferredSize(new Dimension(120, 40));
+		location.setFont(new Font("Arial", Font.PLAIN, 18));
 
 		panel.add(times);
+		panel.add(calc);
 		panel.add(location);
 		window.add(panel);
 
-		printLocationTimes(times, location);
+		printLocationTimes(times, location, calc);
 
 		location.addActionListener (new ActionListener ()
 		{
 			public void actionPerformed(ActionEvent e)
 			{
 				try {
-					printLocationTimes(times, location);
+					printLocationTimes(times, location, calc);
+				}
+				catch(Exception b){System.out.println("Error");}
+			}
+		});
+		
+		calc.addActionListener (new ActionListener ()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				try {
+					printLocationTimes(times, location, calc);
 				}
 				catch(Exception b){System.out.println("Error");}
 			}
 		});
 	}
 
-	public static void printLocationTimes(JLabel times, JComboBox location) throws Exception
+	public static void printLocationTimes(JLabel times, JComboBox location, JComboBox calc) throws Exception
 	{
 		URL url = null;
 		URLConnection urlCon;
 		InputStream is;
 		BufferedReader br;
 
-		String item = (String)location.getSelectedItem();
-		System.out.println(item);
+		String local = (String)location.getSelectedItem();
+		String provider = (String)calc.getSelectedItem();
+		System.out.println(local);
+		System.out.println(provider);
 
-		if(item.equals("Toronto"))
+		if(local.equals("Toronto"))
 		{
-			url = new URL("https://www.muslimpro.com/en/find?coordinates=43.653226%2C-79.3831843&country_code=CA&country_name=Canada&city_name=Toronto&date=&convention=ISNA&asrjuristic=Hanafi");
+			if(provider.equals("ISNA"))
+			{
+				url = new URL("https://www.muslimpro.com/en/find?coordinates=43.653226%2C-79.3831843&country_code=CA&country_name=Canada&city_name=Toronto&date=&convention=ISNA&asrjuristic=Hanafi");
+			}
+			else if(provider.equals("Diyanet"))
+			{
+				url = new URL("https://www.muslimpro.com/en/find?coordinates=43.653226%2C-79.3831843&country_code=CA&country_name=Canada&city_name=Toronto&date=&convention=Diyanet&asrjuristic=Hanafi");
+			}
 		}
-		else if(item.equals("Ottawa"))
+		else if(local.equals("Ottawa"))
 		{
-			url = new URL("https://www.muslimpro.com/en/find?coordinates=45.4215296%2C-75.69719309999999&country_code=CA&country_name=Canada&city_name=Ottawa&date=&convention=ISNA&asrjuristic=Hanafi&highlat=Angle");
+			if(provider.equals("ISNA"))
+			{
+				url = new URL("https://www.muslimpro.com/en/find?coordinates=45.4215296%2C-75.69719309999999&country_code=CA&country_name=Canada&city_name=Ottawa&date=&convention=ISNA&asrjuristic=Hanafi&highlat=Angle");
+			}
+			else if(provider.equals("Diyanet"))
+			{
+				url = new URL("https://www.muslimpro.com/en/find?coordinates=45.4215296%2C-75.69719309999999&country_code=CA&country_name=Canada&city_name=Ottawa&date=&convention=Diyanet&asrjuristic=Hanafi&highlat=Angle");
+			}
 		}
 
 		urlCon = url.openConnection();
@@ -75,15 +112,17 @@ public class PrayerOnline {
 		String line = null;
 		int counter = 0;
 
+		
+		
 		while((line = br.readLine()) != null)
 		{
 			counter++;
-			if(counter == 19)
+			if(counter == 43)
 			{
 				String[] helperArray = line.split("<span class=\"jam-solat\">");
 				for(int i = 0; i < 6; i++)
 				{
-					helperArray[i] = helperArray[i+1].substring(0,5); 
+					helperArray[i] = helperArray[i+1].substring(0,5);
 				}
 
 				times.setText("<html>Fajr-----------" + helperArray[0] +
